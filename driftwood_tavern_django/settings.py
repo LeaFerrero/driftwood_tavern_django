773ebef885a0d15d0e38ea65054956264d75b45c
Leandro Raúl Ferrero <98682483+LeaFerrero@users.noreply.github.com>
2023-12-04 03:32:14 +0000

    Arreglando para deploy

--- driftwood_tavern_django/settings.py
+++ driftwood_tavern_django/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = "static/"
 #    BASE_DIR / "app_menu/static/"
 #]
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
